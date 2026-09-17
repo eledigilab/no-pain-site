12bac81abeccefc212617d2798c0c15754480546
eledigilab <eledigilab@gmail.com>
2026-09-17 10:16:07 +0200

    Modifiche home page, aggiunta contenuti

--- site.js
+++ site.js
@@ -10,7 +10,7 @@
   'use strict';
 
   var LANGS = ['it', 'en', 'fr', 'de', 'es', 'tr', 'el', 'pt', 'ro', 'pl', 'nl', 'sv'];
-  var VERSION = '20260916k';
+  var VERSION = '20260917c';
   var root = document.documentElement;
   window.NP_LANGS = window.NP_LANGS || {};
 
